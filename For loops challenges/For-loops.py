8a9effb3d72afaa36eb0879e9edef47c398c8951
Уляна Федак <ulianafedak@b9897945-e7f6-40ef-b9d4-f97a82c27ad4.fritz.box>
2025-01-22 01:22:33 -0800

    for-loops,3

--- For loops challenges/For-loops.py
+++ For loops challenges/For-loops.py
@@ -1,4 +1,23 @@
+#First task. Writing lines.
 text = "I will listen to my teacher and complete my work on time."
 
 for i in range(10):
     print(text)
+    
+# Second task. Count in 5 from 0 to 500.
+
+num = 0
+for i in range(505):
+    if num % 5 == 0:
+        print (num)
+    num = num + 1
+        
+# Third task. Count down from 100 to 0.
+
+num = 100
+
+while num >= 0:
+    print(num)
+    num = num - 1
+
+# 4th task.
--- For loops challenges/README.md
+++ For loops challenges/README.md
@@ -1,5 +1,7 @@
-### First task
-
-#### Writing Lines!
+### First task. Writing Lines!
 
 - Write down 10 times this text “I will listen to my teacher and complete my work on time.”
+
+### Second task. Count in 5 from 0 to 500.
+
+### Third task. Count down from 100 to 0.
